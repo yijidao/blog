5aee7ce44bb2709ba8c1ad38cd4bf7b50c8a65c2
wengzepeng <wengzepeng@pcitech.com>
2021-09-16 18:48:38 +0800

    demo

---
+++ WPF/MultipleProcessClient/MultipleProcessClient/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultipleProcessClient
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Process> _pluginProcessList = new List<Process>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            load1.Click += (sender, args) =>
+            {
+                var path = $@"{Directory.GetCurrentDirectory()}\Plugin1.dll";
+                var plugin = LoadPlugin(path);
+                region1.Child = plugin;
+            };
+
+            Application.Current.Exit += (sender, args) =>
+            {
+                _pluginProcessList.Where(x => !x.HasExited).ToList().ForEach(x => x.Kill());
+            };
+        }
+
+        private FrameworkElement LoadPlugin(string pluginDll)
+        {
+            using (var pipeServer = new AnonymousPipeServerStream(PipeDirection.In, HandleInheritability.Inheritable))
+            {
+                
+
+                var startInfo = new ProcessStartInfo()
+                {
+                    FileName = "PluginProcess.exe",
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                    Arguments = $"{pluginDll} {pipeServer.GetClientHandleAsString()}"
+                };
+
+                var process = new Process { StartInfo = startInfo };
+                process.Start();
+                _pluginProcessList.Add(process);
+                pipeServer.DisposeLocalCopyOfClientHandle();
+                using (var reader = new StreamReader(pipeServer))
+                {
+                    var handle = new IntPtr(int.Parse(reader.ReadLine()));
+                    return new ViewHost(handle);
+                }
+            }
+        }
+    }
+
+    class ViewHost : HwndHost
+    {
+        private readonly IntPtr _handle;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SetParent(HandleRef hWnd, HandleRef hWndParent);
+
+        public ViewHost(IntPtr handle) => _handle = handle;
+
+        protected override HandleRef BuildWindowCore(HandleRef hwndParent)
+        {
+            SetParent(new HandleRef(null, _handle), hwndParent);
+            return new HandleRef(this, _handle);
+        }
+
+        protected override void DestroyWindowCore(HandleRef hwnd)
+        {
+        }
+
+        
+    }
+}
---
+++ WPF/MultipleProcessClient/PluginProcess/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace PluginProcess
+{
+    class Program
+    {
+        [STAThread]
+        [LoaderOptimization(LoaderOptimization.MultiDomain)]
+        static void Main(string[] args)
+        {
+            if (args.Length != 2) return;
+
+            var dllPath = args[0];
+            var serverHandle = args[1];
+            var dll = Assembly.LoadFile(dllPath);
+            var startupType = dll.GetType($"{dll.GetName().Name}.PluginStartup");
+            var startup = Activator.CreateInstance(startupType);
+            var view =(FrameworkElement)  startupType.GetMethod("CreateView").Invoke(startup, null);
+            
+
+            using (var pipeCline = new AnonymousPipeClientStream(PipeDirection.Out, serverHandle))
+            {
+                using (var writer = new StreamWriter(pipeCline))
+                {
+                    writer.AutoFlush = true;
+                    var handle = ViewToHwnd(view);
+                    writer.WriteLine(handle.ToInt32());
+                }
+            }
+            Dispatcher.Run();
+        }
+
+        private static IntPtr ViewToHwnd(FrameworkElement element)
+        {
+            var p = new HwndSourceParameters()
+            {
+                ParentWindow = new IntPtr(-3),
+                WindowStyle = 1073741824
+            };
+            var hwndSource= new HwndSource(p)
+            {
+                RootVisual = element,
+                SizeToContent = SizeToContent.Manual,
+            };
+            hwndSource.CompositionTarget.BackgroundColor = Colors.White;
+            return hwndSource.Handle;
+        }
+    }
+
+
+
+}
