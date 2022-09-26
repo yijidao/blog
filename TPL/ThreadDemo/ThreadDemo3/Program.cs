f1705cd69d1254d00877a648bb4bc752d488fc65
wengzepeng <wengzepeng@pcitech.com>
2022-09-26 18:08:41 +0800

    demo

---
+++ TPL/ThreadDemo/ThreadDemo3/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace ThreadDemo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var volatileTest = new VolatileTest();
+            volatileTest.Call();
+            
+        }
+
+        
+
+    }
+}
---
+++ TPL/ThreadDemo/ThreadDemo3/VolatileTest.cs
@@ -0,0 +1,28 @@
+﻿namespace ThreadDemo3;
+
+class VolatileTest
+{
+    public void Call()
+    {
+        var t = new Thread(Test);
+        t.Start();
+
+        while (!Flag)
+        {
+
+        }
+
+        Console.WriteLine("Hello, World!");
+        Console.ReadLine();
+
+    }
+    private  bool Flag = false; // 需要添加 volatile
+
+    void Test()
+    {
+        Thread.Sleep(500);
+        Flag = true; // 在 Release 模式下会被优化掉，导致 Main() 无法执行到 Hello World
+        Console.WriteLine("退出线程");
+
+    }
+}
